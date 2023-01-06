658d6da2c1eb9b013f1028b9835880f19143f777
manski117 <choromanski117@gmail.com>
2023-01-06 17:14:35 -0500

    Add Ships factory and tests
    Factories.tests.js successfully is able to test for properties in
    the ship objects.

---
+++ factories.js
@@ -0,0 +1,64 @@
+let ShipLengths = {
+    'carrier': 5,
+    'battleship': 4,
+    'cruiser': 3,
+    'submarine': 3,
+    'destroyer': 2
+}
+
+//ships
+const Ships = (type) => {
+    //property vars
+    const shipType = type;
+    const length = ShipLengths[type];
+    let direction = 'vertical';
+    let timesHit = 0;
+    let sunk = false;
+
+    //methods
+    function hit(){
+        //increases timesHit
+    }
+
+    function isSunk(){
+        //if hit = length then make true. 
+    }
+    
+
+    //return line
+    return {shipType, length, direction, timesHit, sunk, hit, isSunk};
+};
+
+
+
+//gameboard
+
+const Factory = (arg) => {
+    
+  
+    return {functions};
+};
+
+
+
+
+
+
+
+
+
+
+
+
+function sum4(a, b) {
+    return a + b;
+  }
+  
+function subtract4(a, b) {
+    return a - b;
+}
+
+
+
+//////Test export line/////
+module.exports = {sum4, subtract4, Ships};
---
+++ factories.test.js
@@ -0,0 +1,19 @@
+const {sum4, subtract4, Ships} = require('./factories');
+// import {sum, subtract, compileAndroidCode} from './main';
+// import sum from './main';
+// import subtract from './main';
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum4(1, 2)).toBe(3);
+});
+
+test('adds 10 - 6 to equal 4', () => {
+    expect(subtract4(10, 6)).toBe(4);
+});
+
+
+let testShip = Ships('destroyer');
+test('length property', () => {
+    
+    expect(testShip.length).toBe(2);
+});
