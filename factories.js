7d40e0a89dc03af0bf813117197870101e551eb8
manski117 <choromanski117@gmail.com>
2023-01-07 13:43:15 -0500

    Add gameboard constructor to Gameboard
    Gameboard factory now builds gameboard as a 2D array 10x10 with
    null in each element as default. All tests related to board pass.

--- factories.js
+++ factories.js
@@ -16,22 +16,116 @@ const Ship = (type) => {
     let sunk = false;
 
     //methods
+    function getTimesHit(){
+        return timesHit;
+    }
+
+    function getSunk(){
+        return sunk;
+    }
+
+    function getDirection(){
+        return direction;
+    }
+
+    function changeDirection(){
+        if (direction === 'vertical'){
+            direction = 'horizontal'
+        }
+        if (direction === 'horizontal'){
+            direction = 'vertical'
+        }
+    }
+
     function hit(){
         //increases timesHit
+        timesHit++;
+        console.log(timesHit);
+        isSunk();
     }
 
     function isSunk(){
-        //if hit = length then make true. 
+        //logic for ship health
+        if(getTimesHit() < length){
+            return false;
+        } else if(getTimesHit() >= length){
+            sinkShip();
+            return true;
+        }
     }
+
+    function sinkShip(){
+        sunk = true;
+        return;
+    }
+
+    
+
+    //add getter
+
     
 
     //return line
-    return {shipType, length, direction, timesHit, sunk, hit, isSunk};
+    return {shipType, length, direction, hit, isSunk, getTimesHit, getSunk, sinkShip, getDirection, changeDirection};
 };
 
 
 
 //gameboard
+
+const Gameboard = (arg) => {
+    //properties
+    let missedAttacks = 0;
+    //create a 10 x 10 game board, coordinate style board[row][col];
+    //map takes the 10 null elements and turns them into 10 arrays of null elements. 
+    let board = Array(10).fill(null).map(() => Array(10).fill(null));
+
+    function getBoard(){
+        return board;
+    }
+
+    function placeShip(shipObj, x, y){
+        //takes a ship object, the row coordinate number, and the column coordinate number
+        let name = shipObj.shipType;
+        let direction = shipObj.getDirection();
+        //call the ship function to make ships
+
+        //check if placement is valid
+        if (validSpace()){
+            //places them at specific coordinates
+            //get the ship type 
+            board[y][x] = {ship.shipType, }
+
+        }
+            
+
+        //if not throw error or try/catch
+    }
+
+    function validSpace(x =0, y = 0, direction = 'vertical'){
+        return true;
+    }
+
+    function receiveAttack(coords){
+        //takes a pair of coordinates
+        //determine if ship is there
+            //send hit function to the correct ship
+            //OR record coordinates of missed shots
+    }
+
+    function displayHit(coord){
+        //takes a coord and displays on board where that shot was for player.
+        //hits and misses should look different.
+    }
+
+    function areAllShipSunk(){
+        //if all ships are sunk, true
+        //if not, false
+    }
+  
+    return {getBoard};
+};
+
 
 const Factory = (arg) => {
     
@@ -61,4 +155,4 @@ function subtract4(a, b) {
 
 
 //////Test export line/////
-module.exports = {sum4, subtract4, Ship};
+module.exports = {sum4, subtract4, Ship, Gameboard};
--- factories.test.js
+++ factories.test.js
@@ -1,4 +1,6 @@
-const {sum4, subtract4, Ship} = require('./factories');
+// const { default: test } = require('node:test');
+// const { default: test } = require('node:test');
+const {sum4, subtract4, Ship, Gameboard} = require('./factories');
 // import {sum, subtract, compileAndroidCode} from './main';
 // import sum from './main';
 // import subtract from './main';
@@ -12,8 +14,174 @@ test('adds 10 - 6 to equal 4', () => {
 });
 
 
-let testShip = Ship('destroyer');
-test('length property', () => {
-    
-    expect(testShip.length).toBe(2);
+
+
+describe.skip('Ship factory function tests', () => {
+
+
+    describe('Ship factory properties', () => {
+        let testShip = Ship('destroyer');
+
+        test('length property', () => {
+            expect(testShip.length).toBe(2);
+        });
+
+        test('type property', () => {
+            expect(testShip.shipType).toBe('destroyer');
+        });
+
+        test('direction property', () => {
+            testShip.direction = 'horizontal'
+            expect(testShip.direction).toBe('horizontal');
+        });
+
+        test('timesHit property', () => {
+            expect(testShip.getTimesHit()).toBe(0);
+        });
+
+        test('sunk property not sunk', () => {
+            expect(testShip.getSunk()).toBe(false);
+        });
+        test('sunk property sank', () => {
+            testShip.hit();
+            testShip.hit();
+            expect(testShip.getSunk()).toBe(true);
+        });
+
+        
+
+
+    });
+
+    describe('Ship factory methods', () => {
+        
+        test('hit function', () => {
+            let testShip = Ship('destroyer');
+            testShip.hit();
+            expect(testShip.getTimesHit()).toBe(1);
+        });
+
+        test('isSunk function negative', () => {
+            let testShip = Ship('destroyer');
+            testShip.hit();
+            
+            expect(testShip.isSunk()).toBe(false);
+        });
+        test('isSunk function positive', () => {
+            let testShip = Ship('destroyer');
+            testShip.hit();
+            testShip.hit();
+            expect(testShip.isSunk()).toBe(true);
+        });
+
+        
+
+
+    });
+
+
+
+});
+
+
+describe('GameBoard factory function tests', () => {
+
+
+    describe('Game Board attributes', () => {
+        let testBoard = Gameboard();
+
+        //test empty board
+        test('Is board empty when initialized?', () => {
+            //tests if every element of every subarr of every arr passes test
+            let testValue = testBoard.getBoard().every((row) => row.every((space) => space === null));
+            expect(testValue).toBe(true);
+        });
+
+        //test board row length
+        test('Row length', () =>{
+            let testValue = testBoard.getBoard().length;
+            expect(testValue).toEqual(10);
+        });
+
+        //test board column length
+        test('Column length', () =>{
+            let testValue = testBoard.getBoard()[0].length;
+            expect(testValue).toEqual(10);
+        });
+
+    });
+
+    describe('Place ship horizontal', () => {
+        let testBoard = Gameboard();
+        
+       //test if a ships indexes are at the expected places when placed horz
+       test('place ship at first square', () =>{
+        let testShip = Ship('carrier');
+        testBoard.palceShip(testShip, 0, 0);
+        let testValue = testBoard.getBoard()[0][0].shipName;
+        expect(testValue).toBe('carrier');
+
+       });
+
+       test('place ship index at first square', () =>{
+        let testShip = Ship('carrier');
+        testBoard.palceShip(testShip, 0, 0);
+        let testValue = testBoard.getBoard()[0][0].shipIdex = 0;
+        expect(testValue).toEqual(0);
+
+       });
+
+    });
+
+    describe('Place ship vertical', () => {
+        
+        //test if a ships indexes are at the expected places when placed vert
+ 
+    });
+
+    describe('Do NOT place ship out-of-bounds', () => {
+        
+        //test vert and horz
+ 
+    });
+
+    describe('Do NOT make ship collide with another ship', () => {
+        
+        //test vert and horz
+ 
+    });
+
+    describe('All ships placed', () => {
+        
+        //test no ships, some ships, and all ships
+ 
+    });
+
+
+    describe('Recieve attack tests', () => {
+        
+        //test to see if ships get hit in the expected place
+
+        //test record hits
+
+        //test record misses
+
+        //test targeting a space that has already been shot
+ 
+    });
+
+    describe('All ships sunk', () => {
+        
+        //test no ships, some ships, and all ships sunk
+ 
+    });
+
+    describe('reset board', () => {
+        
+        //make a new board, run clear function, and see if things are empty as expected
+ 
+    });
+
+
+
 });
